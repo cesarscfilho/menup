d48ca61c7f101d73cf75a3e5a6c0054c09cd93aa
cesarscfilho <cesarscfilho@gmail.com>
2024-01-06 12:45:57 -0300

    chore: marketing -> web

--- apps/app/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import Image from "next/image";
-import { Button } from "@menup/ui";
-
-export default function Home() {
-  return <Button>Fake button</Button>;
-}
--- apps/marketing/tailwind.config.ts
+++
@@ -1,3 +0,0 @@
-const config = require("@menup/ui/tailwind.config");
-
-module.exports = config;
