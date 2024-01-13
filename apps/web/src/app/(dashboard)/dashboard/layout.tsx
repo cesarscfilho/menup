1ff3b02ef47f0c4c116480303a4495f120b15808
Cesar Silva <cesarscfilho@gmail.com>
2024-01-12 21:01:09 -0300

    feat(dashboard): init dashboard layout

--- apps/web/src/app/(dashboard)/dashboard/layout.tsx
+++ apps/web/src/app/(dashboard)/dashboard/layout.tsx
@@ -1,3 +1,18 @@
-export default function DashboardLayout() {
-  return <div>Dashboard</div>;
+import DashboardSidebar from "@/components/layout/dashboard-sidebar";
+
+interface DashboardLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function DashboardLayout({ children }: DashboardLayoutProps) {
+  return (
+    <div className="flex min-h-screen flex-col">
+      <div className="container flex-1 items-start md:grid md:grid-cols-[220px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[240px_minmax(0,1fr)] lg:gap-10">
+        <DashboardSidebar />
+        <main className="bg-background flex w-full flex-col overflow-hidden">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
 }
