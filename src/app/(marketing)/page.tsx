8ddf3ea339bbef3c05ead0fcc04e2bc71a69850f
cesarscfilho <cesarscfilho@gmail.com>
2024-01-22 23:48:39 -0300

    feat(marketing): update button

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -34,7 +34,12 @@ export default async function Index() {
               <Icons.playcircle className="mr-2 h-4 w-4" />
               Demo
             </Button>
-            <Button>Start for free</Button>
+            <button className="group relative inline-flex h-9 items-center justify-center overflow-hidden rounded-md bg-neutral-950 px-6 font-medium text-neutral-200 transition hover:scale-110">
+              <span>Start for free</span>
+              <div className="absolute inset-0 flex h-full w-full justify-center [transform:skew(-12deg)_translateX(-100%)] group-hover:duration-1000 group-hover:[transform:skew(-12deg)_translateX(100%)]">
+                <div className="relative h-full w-8 bg-white/20"></div>
+              </div>
+            </button>
           </div>
         </div>
       </section>
