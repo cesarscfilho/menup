599696dc175a1816a28d3fd59af71dc6c7968f16
cesarscfilho <cesarscfilho@gmail.com>
2024-02-14 13:57:32 -0300

    refactor: swap tabs on the product page and add-ons for different routes

---
+++ src/app/(dashboard)/dashboard/[storeId]/products/[productId]/addons/page.tsx
@@ -0,0 +1,99 @@
+import { db } from '@/db'
+import {
+  addons,
+  AddonsCategory,
+  addonsCategory,
+  productsCategoryAddons,
+} from '@/db/schema'
+import { and, eq } from 'drizzle-orm'
+
+import { Container } from '@/components/container'
+import { InfoCard } from '@/components/info-card'
+import { LinkTabs } from '@/components/pagers/link-tabs'
+
+interface ProductAddonsPageProps {
+  params: {
+    productId: string
+  }
+}
+
+export default async function ProductAddonsPage({
+  params,
+}: ProductAddonsPageProps) {
+  const { productId } = params
+
+  const productAddons = await db
+    .select({
+      category: {
+        id: addonsCategory.id,
+        name: addonsCategory.name,
+      },
+      items: {
+        id: addons.id,
+        name: addons.name,
+      },
+    })
+    .from(addonsCategory)
+    .where(eq(addonsCategory.productId, productId))
+    .leftJoin(
+      productsCategoryAddons,
+      and(
+        eq(productsCategoryAddons.productId, productId),
+        eq(productsCategoryAddons.addonsCategoryId, addonsCategory.id),
+      ),
+    )
+    .leftJoin(addons, eq(addons.id, productsCategoryAddons.addonsId))
+    .then((res) => {
+      const items = res.reduce<
+        Record<
+          string,
+          {
+            name: string
+            id: string
+            items: Pick<AddonsCategory, 'id' | 'name'>[]
+          }
+        >
+      >((acc, row) => {
+        const category = row.category
+        const item = row.items
+
+        if (!acc[category.name]) {
+          acc[category.name] = {
+            ...category,
+            items: [],
+          }
+        }
+
+        if (item) {
+          acc[category.name].items.push(item)
+        }
+
+        return acc
+      }, {})
+
+      return Object.values(items)
+    })
+
+  return (
+    <Container className="my-8 space-y-4">
+      <LinkTabs productId={productId} />
+
+      <div className="mb-5 flex items-center space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Product addons</h2>
+      </div>
+
+      {productAddons.length < 1 ? (
+        <InfoCard heading="This product dons't have addons yet" />
+      ) : null}
+
+      {productAddons.map((item) => (
+        <div key={item.id}>
+          {item.name} =
+          {item.items.map((item) => (
+            <div key={item.id}>{item.name}</div>
+          ))}
+        </div>
+      ))}
+    </Container>
+  )
+}
--- src/app/(dashboard)/dashboard/[storeId]/products/[productId]/page.tsx
+++ src/app/(dashboard)/dashboard/[storeId]/products/[productId]/page.tsx
@@ -1,15 +1,11 @@
+import { notFound } from 'next/navigation'
 import { db } from '@/db'
-import {
-  addons,
-  AddonsCategory,
-  addonsCategory,
-  productsCategoryAddons,
-} from '@/db/schema'
-import { and, eq } from 'drizzle-orm'
+import { categories, products } from '@/db/schema'
+import { eq } from 'drizzle-orm'
 
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Container } from '@/components/container'
-import { InfoCard } from '@/components/info-card'
+import { UpdateProductForm } from '@/components/forms/update-product-form'
+import { LinkTabs } from '@/components/pagers/link-tabs'
 
 interface ProductPageProps {
   params: {
@@ -20,92 +16,27 @@ interface ProductPageProps {
 export default async function ProductPage({ params }: ProductPageProps) {
   const { productId } = params
 
-  const productAddons = await db
-    .select({
-      category: {
-        id: addonsCategory.id,
-        name: addonsCategory.name,
-      },
-      items: {
-        id: addons.id,
-        name: addons.name,
-      },
-    })
-    .from(addonsCategory)
-    .where(eq(addonsCategory.productId, productId))
-    .leftJoin(
-      productsCategoryAddons,
-      and(
-        eq(productsCategoryAddons.productId, productId),
-        eq(productsCategoryAddons.addonsCategoryId, addonsCategory.id),
-      ),
-    )
-    .leftJoin(addons, eq(addons.id, productsCategoryAddons.addonsId))
-    .then((res) => {
-      const items = res.reduce<
-        Record<
-          string,
-          {
-            name: string
-            id: string
-            items: Pick<AddonsCategory, 'id' | 'name'>[]
-          }
-        >
-      >((acc, row) => {
-        const category = row.category
-        const item = row.items
+  const product = await db.query.products.findFirst({
+    where: eq(products.id, productId),
+  })
 
-        if (!acc[category.name]) {
-          acc[category.name] = {
-            ...category,
-            items: [],
-          }
-        }
+  if (!product) {
+    notFound()
+  }
 
-        if (item) {
-          acc[category.name].items.push(item)
-        }
-
-        return acc
-      }, {})
-
-      return Object.values(items)
-    })
+  const allCategories = await db
+    .select()
+    .from(categories)
+    .where(eq(categories.storeId, product.storeId))
 
   return (
-    <Container className="my-8">
-      <Tabs defaultValue="product" className="space-y-4">
-        <TabsList>
-          <TabsTrigger value="product">Product</TabsTrigger>
-          <TabsTrigger value="variants">Variants</TabsTrigger>
-        </TabsList>
-        <TabsContent value="product">
-          <div className="mb-5 flex items-center space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">
-              Update product
-            </h2>
-          </div>
+    <Container className="my-8 space-y-4">
+      <LinkTabs productId={product.id} />
+      <div className="mb-5 flex items-center space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Update product</h2>
+      </div>
 
-          {/* <UpdateProductForm product={product} categories={allCategories} /> */}
-        </TabsContent>
-        <TabsContent value="variants">
-          <div className="mb-5 flex items-center gap-2 space-y-2">
-            <h2 className="text-3xl font-bold tracking-tight">Variants</h2>
-          </div>
-          {productAddons.length < 1 ? (
-            <InfoCard heading="This product dons't have variants yet" />
-          ) : null}
-
-          {productAddons.map((item) => (
-            <div key={item.id}>
-              {item.name} =
-              {item.items.map((item) => (
-                <div key={item.id}>{item.name}</div>
-              ))}
-            </div>
-          ))}
-        </TabsContent>
-      </Tabs>
+      <UpdateProductForm product={product} categories={allCategories} />
     </Container>
   )
 }
